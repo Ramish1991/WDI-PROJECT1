9fe807f53656159c5e265271d197be74d6055003
ramish1991 <arona000@hotmail.com>
2015-10-13 09:27:08 +0100

    Red to white fade out added

---
+++ app.js
@@ -0,0 +1,41 @@
+
+
+$(function (){
+	$(".box").on("click", function(){
+
+		var boxWhichWasClicked = $( this );
+		console.log(boxWhichWasClicked.attr("id") + ' works!');
+
+
+		// $(boxWhichWasClicked).animate({"background-color", "blue"});
+		// boxWhichWasClicked.addClass("noColour");
+		// boxWhichWasClicked.removeClass("noColour");
+
+	// $(boxWhichWasClicked).css("background-color", "red");
+	// p.show(1500);
+	$(boxWhichWasClicked).css("background-color", "red");
+
+	boxWhichWasClicked.delay(100).queue(function() {
+		$(boxWhichWasClicked).css("background-color", "white");
+	  });
+
+	});
+})
+
+
+$(function hideAllBoxes() {
+
+	var allBoxes = $(".box");
+
+	for (var i = allBoxes.length - 1; i >= 0; i--) {
+
+		var box = allBoxes[i];
+
+		// var p = $(box).css("background-color", "yellow");
+		// p.hide;
+
+	}; 
+
+})
+
+
