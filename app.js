86829f69a689822973feae38b46792556b58ce48
ramish1991 <arona000@hotmail.com>
2015-10-13 16:53:43 +0100

    Game almost complete. Css Remaining. Ending Alert displaying winner remaining.

--- app.js
+++ app.js
@@ -1,72 +1,120 @@
 $(document).ready(function() {
 
 //Make Player1 Choose boxes.
-function playerOnesPiks() {
-// counter meant to allow 4 picks for Player1
-var counter = 1
-for (var i = 0; i < 4; i++) {
-	// from below
-	var cells = $("td:not(.selected)")
+// function playerOnesPiks() {
+// // counter meant to allow 4 picks for Player1
+// var counter = 1
+// for (var i = 0; i < 4; i++) {
+// 	// from below
+// 	var cells = $("td:not(.selected)")
 
-	var playerOnesArrayChoices = []
+// 	var playerOnesArrayChoices = []
 
-	playerOnesArrayChoices.push("playerOneChoice");
-// on clicking, player will define his piks with cell1 / cell2 / cell3 / cell4
-var playerOneChoice = $("td").on("click", function(){
-	addClass("cell" + counter);
-	counter++
+// 	playerOnesArrayChoices.push("playerOneChoice");
+// // on clicking, player will define his piks with cell1 / cell2 / cell3 / cell4
+// var playerOneChoice = $("td").on("click", function(){
+// 	addClass("cell" + counter);
+// 	counter++
 
-	var cells = $("td:(.playersSelect)")
-	var currentCell = $(cells[counter])
-	currentCell.addClass("cell" + counter)
-})
+// 	var cells = $("td:(.playersSelect)")
+// 	var currentCell = $(cells[counter])
+// 	currentCell.addClass("cell" + counter)
+// })
 
-}}
+// }}
 
-playerOnesPiks();
-console.log("playerOneChoice")
+// playerOnesPiks();
+// console.log("playerOneChoice")
+// });
+
+var playerOneScore = 0
+var playerTwoScore = 0
+
+var currentPlayer = 1 
+var round = 1
+
+var playerOneScoreElement = $('.score.player1')
+var playerTwoScoreElement = $('.score.player2') 
+
+ function pickCells() {
+
+ 	var counter = 1
+
+ 	for (var i = 0; i < 4; i++) {
+
+ 		setTimeout(function() {
+
+ 			var cells = $("td:not(.selected)")
+ 			var randomIndex = Math.floor(Math.random() * (cells.length-1))
+
+ 			var currentCell = $(cells[randomIndex])
+			currentCell.addClass("cell" + counter)
+			counter++
+
+			currentCell.addClass("selected")
+		}, i * 1000);
+	}
+	setTimeout(function(){
+		$("td.selected").removeClass("selected")
+	}, 4000)
+}
+
+pickCells();
+
+function resetBoard() {
+	$("td").removeClass("selected cell1 cell2 cell3 cell4");
+	userSelect = 1
+	pickCells(); // pick new cells
+	// change player
+	if (currentPlayer === 1) {
+		currentPlayer = 2
+	} else  {
+		currentPlayer = 1					
+	}
+	if (round === 6){
+		alert("Game Over");
+	}
+
+	round++
+}
+
+var userSelect = 1;
+
+$("td").on("click", function(){
+console.log(currentPlayer)
+	var cell = $(this)
+	if(cell.hasClass("cell"+ userSelect)){
+		if(userSelect === 4) {
+			
+			setTimeout(
+				function(){
+
+					if (currentPlayer === 1){
+						playerOneScore++
+					} else {
+						playerTwoScore++
+					}
+					playerOneScoreElement.text("Player 1 Score = " + playerOneScore)
+					playerTwoScoreElement.text("Player 2 Score = " + playerTwoScore)
+					
+					resetBoard();
+					console.log(playerOneScore, playerTwoScore)
+				},
+				2000
+			)
+
+		}
+		userSelect++
+		cell.addClass("selected")
+	} else {
+		alert("You lost!")
+		resetBoard();
+	}
+
 });
 
-//+ function pickCells() {
 
-//+ 	var counter = 1
-
-//+ 	for (var i = 0; i < 4; i++) {
-
-//- 		setTimeout(function() {
-
-//+ 			var cells = $("td:not(.selected)")
-//- 			var randomIndex = Math.floor(Math.random() * (cells.length-1))
-
-//+ 			var currentCell = $(cells[randomIndex])
-// 			currentCell.addClass("cell" + counter)
-// 			counter++
-
-// 			currentCell.addClass("selected")
-// 		}, i * 1000);
-// 	}
-// 	setTimeout(function(){
-// 		$("td.selected").removeClass("selected")
-// 	}, 4000)
-// }
-
-// pickCells();
-
-// var userSelect = 1;
-
-// $("td").on("click", function(){
-// 	var cell = $(this)
-// 	if(cell.hasClass("cell"+ userSelect)){
-// 		userSelect++
-// 		cell.addClass("selected")
-// 	} else {
-// 		alert("You lost!")
-// 	}
-// 		// do stuff on click
-// 	});
-
-
-// });
+});
 
 
 
