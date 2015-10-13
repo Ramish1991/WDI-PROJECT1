f581585bb11dc0885732e70de9d824a90d738771
ramish1991 <arona000@hotmail.com>
2015-10-13 15:04:24 +0100

    Attempt at Player1'sPicks. Not saved the picks. Commented out rest.

--- app.js
+++ app.js
@@ -1,58 +1,105 @@
 $(document).ready(function() {
 
-	// function hideAllBoxes() {
+//Make Player1 Choose boxes.
+function playerOnesPiks() {
+// counter meant to allow 4 picks for Player1
+	var playersSelect = 1
+	for (var i = 0; i < 4; i++) {
+// on clicking, player will define his piks with cell1 / cell2 / cell3 / cell4
+	var playerOneChoice = $("td").on("click", function(){
+		addClass("cell" + playersSelect);
+		playersSelect++
 
-	// 	var allBoxes = $(".box");
+		$("td").on("click", function(){
+			var playerOnesPik = $("td").addClass()
+	}
 
-	// 	for (var i = 0; i < allBoxes.length; i++) {
+	)})
+}
+}
 
-	// 		var box = allBoxes[i];
+})
 
-	// 		setTimeout(function(box){
-	// 			$(box).show()
-	// 		}, 1000*i, box);
-	// 	}
-	// };
 
-	function pickCells() {
-		
-		var counter = 1
+// playerOnesPiks();
 
-    for (var i = 0; i < 4; i++) {
+// function pickCells() {
 
-    	setTimeout(function() {
+// 	var counter = 1
 
-	    	var cells = $("td:not(.selected)")
-	  		var randomIndex = Math.floor(Math.random() * (cells.length-1))
+// 	for (var i = 0; i < 4; i++) {
 
-				var currentCell = $(cells[randomIndex])
-		    currentCell.addClass("cell" + counter)
-		    counter++
+// 		setTimeout(function() {
 
-		    currentCell.addClass("selected")
-		  }, i * 1000);
-    }
-    setTimeout(function(){
-    	$("td.selected").removeClass("selected")
-    }, 4000)
-  }
+// 			var cells = $("td:not(.selected)")
+// 			var randomIndex = Math.floor(Math.random() * (cells.length-1))
 
-	pickCells();
+// 			var currentCell = $(cells[randomIndex])
+// 			currentCell.addClass("cell" + counter)
+// 			counter++
 
-	var userSelect = 1;
+// 			currentCell.addClass("selected")
+// 		}, i * 1000);
+// 	}
+// 	setTimeout(function(){
+// 		$("td.selected").removeClass("selected")
+// 	}, 4000)
+// }
 
-	$("td").on("click", function(){
-		var cell = $(this)
-		if(cell.hasClass("cell"+ userSelect)){
-			userSelect++
-			cell.addClass("selected")
-		} else {
-			console.log("End Game")
-		}
-		// do stuff on click
 
-	});
-});
+
+// pickCells();
+
+
+
+
+// var userSelect = 1;
+
+// $("td").on("click", function(){
+// 	var cell = $(this)
+// 	if(cell.hasClass("cell"+ userSelect)){
+// 		userSelect++
+// 		cell.addClass("selected")
+// 	} else {
+// 		alert("You lost!")
+// 	}
+// 		// do stuff on click
+// 	});
+
+
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// function hideAllBoxes() {
+
+// 	var allBoxes = $(".box");
+
+// 	for (var i = 0; i < allBoxes.length; i++) {
+
+// 		var box = allBoxes[i];
+
+// 		setTimeout(function(box){
+// 			$(box).show()
+// 		}, 1000*i, box);
+// 	}
+// };
 
 // Execute hideAllBoxes
 	// hideAllBoxes();
@@ -99,7 +146,7 @@ $(document).ready(function() {
 
 // need to grab all cells and randomize the order before showing them
 // then store the ID of the boxes shown in an array to save the order, and compare to the user choice 
-	
+
 // });
 		// $(box).on(click. function(){
 
