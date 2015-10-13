5335c8f76ea98f35e5b3b02b4d6cdb24b5a2c284
ramish1991 <arona000@hotmail.com>
2015-10-13 11:08:57 +0100

    Cleaned up. HideAllBoxes put to top. $ removed from places.

--- app.js
+++ app.js
@@ -1,37 +1,43 @@
 $(document).ready(function() {
 
-	$(function (){
-		$(".box").on("click", function(){
-
-		var boxWhichWasClicked = $(this);
-		console.log(boxWhichWasClicked.attr("id") + ' works!');
 
 
-		// $(boxWhichWasClicked).animate({"background-color", "blue"});
-		// boxWhichWasClicked.addClass("noColour");
-		// boxWhichWasClicked.removeClass("noColour");
-
-	  // $(boxWhichWasClicked).css("background-color", "red");
-	  // p.show(1500);
-	  $(boxWhichWasClicked).css("background-color", "red");
-
-	   boxWhichWasClicked.delay(100).queue(function() {
-		$(boxWhichWasClicked).css("background-color", "white");
-	});
-
-});
-	})
-
-	$(function hideAllBoxes() {
+	function hideAllBoxes() {
 
 		var allBoxes = $(".box");
 
-		for (var i = allBoxes.length - 1; i >= 0; i--) {
+		for (var i = 0; i < allBoxes.length; i++) {
 
 			var box = allBoxes[i];
-			$(box).show.Math.random()
 
-}});
+			setTimeout(function(box){
+				$(box).show()
+			}, 1000*i, box);
+		}
+	};
+
+	hideAllBoxes();
+
+	$(".box").on("click", function(){
+
+		var boxWhichWasClicked = $(this);
+		console.log(boxWhichWasClicked.attr("id") + ' works!')
+
+	  boxWhichWasClicked.delay(100).queue(function() {
+			$(boxWhichWasClicked).css("background-color", "white");
+		})
+
+})
+
+
+
+	// to create the sequence showing different boxes, 
+	// need to use setInterval and then store the ID of the boxes shown in an array to save the order, and compare to the user choice 
+
+	
+
+	
+});
 		// $(box).on(click. function(){
 
 		// })
@@ -42,4 +48,11 @@ $(document).ready(function() {
 // 	$(box).show.math.random()
 // });
 
-});
+
+
+
+	// $(boxWhichWasClicked).animate({"background-color", "blue"});
+	// boxWhichWasClicked.addClass("noColour");
+	// boxWhichWasClicked.removeClass("noColour");
+  // p.show(1500);
+// $(boxWhichWasClicked).css("background-color", "red");
