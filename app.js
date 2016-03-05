6bb8d6b8459718c2c68176f13404570e66cb72f6
Ramish1991 <arona000@hotmail.com>
2016-03-05 23:28:17 +0000

    Fixed issue with repeat images appearing

--- app.js
+++ app.js
@@ -1,42 +1,49 @@
 $(document).ready(function() {
 
-var playerOneScore = 0
-var playerTwoScore = 0
+	var playerOneScore = 0
+	var playerTwoScore = 0
 
-var currentPlayer = 1 
-var round = 1
+	var currentPlayer = 1 
+	var round = 1
 
-var playerOneScoreElement = $('.score.player1')
-var playerTwoScoreElement = $('.score.player2') 
+	var playerOneScoreElement = $('.score.player1')
+	var playerTwoScoreElement = $('.score.player2') 
 
- function pickCells() {
+	function pickCells() {
 
- 	var counter = 1
+		var counter = 1
 
- 	for (var i = 0; i < 4; i++) {
+		for (var i = 0; i < 4; i++) {
 
- 		setTimeout(function() {
+ 		setTimeout(function() { // LETS TD CELLS APPEAR EACH BY EACH
 
  			var cells = $("td:not(.selected)")
  			var randomIndex = Math.floor(Math.random() * (cells.length-1))
 
  			var currentCell = $(cells[randomIndex])
-			currentCell.addClass("cell" + counter)
-			counter++
+ 			currentCell.addClass("cell" + counter)
+ 			counter++
 
-			currentCell.addClass("selected")
-		}, i * 1000);
-	}
-	setTimeout(function(){
-		$("td.selected").removeClass("selected")
-	}, 4000)
-}
+ 			currentCell.addClass("selected")
+ 		}, i * 1000);
+ 	}
+ 	setTimeout(function(){
+ 		$("td.selected").removeClass("selected")
+ 	}, i * 1000)
+ }
 
-pickCells();
+// START BUTTON IS SWITCHED TO RESET BUTTON
+ document.getElementById('begin-btn').onclick = function() {
+ 	pickCells();
+ 	$('#begin-btn').attr('id', 'reset-btn')
+ 	document.getElementById('reset-btn').onclick = function() {
+ 		resetBoard();
+ 	}
+ }
 
-function resetBoard() {
-	$("td").removeClass("selected cell1 cell2 cell3 cell4");
-	userSelect = 1
+ function resetBoard() {
+ 	$("td").removeClass("selected cell1 cell2 cell3 cell4");
+ 	userSelect = 1
 	pickCells(); // pick new cells
 	// change player
 	if (currentPlayer === 1) {
@@ -60,7 +67,7 @@ function resetBoard() {
 var userSelect = 1;
 
 $("td").on("click", function(){
-console.log(currentPlayer)
+	console.log(currentPlayer)
 	var cell = $(this)
 	if(cell.hasClass("cell"+ userSelect)){
 		if(userSelect === 4) {
@@ -70,19 +77,19 @@ console.log(currentPlayer)
 
 					if (currentPlayer === 1){
 						playerOneScore++
-						alert("Player 1 scored " + playerOneScore)
+						// alert("Player 1 scored " + playerOneScore)
 					} else {
 						playerTwoScore++
-						alert("Player 2 scored " + playerTwoScore)
+						// alert("Player 2 scored " + playerTwoScore)
 					}
-					playerOneScoreElement.text("Player 1 Score = " + playerOneScore)
-					playerTwoScoreElement.text("Player 2 Score = " + playerTwoScore)
+					playerOneScoreElement.text(playerOneScore)
+					playerTwoScoreElement.text(playerTwoScore)
 					
 					resetBoard();
 					console.log("playerOneScore", "playerTwoScore")
 				},
 				2000
-			)
+				)
 
 		}
 		userSelect++
